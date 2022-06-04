fcdd1cacf810d9a3bf06747b368aed192377b67d
Sharon Mwenda <smkathambi@gmail.com>
2022-06-04 13:38:40 +0300

    commit tree 8e30745a7e0fe4531e99df7e83b4ae99b72b5f13

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const cloneUpdateEmployee = {...obj};
+    cloneUpdateEmployee[key]= value;
+    return cloneUpdateEmployee;
+}
+updateEmployeeWithKeyAndValue();
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+const updateAddress = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Broadway");
+function deleteFromEmployeeByKey(obj, key) {
+    const newCloneUpdateEmployee = {...obj};
+    delete newCloneUpdateEmployee.name;
+    return newCloneUpdateEmployee;
+
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete employee.name;
+    return obj;
+}
